99c2036bdc40c94f28222435978654ae802f923f
zach beecher <zachbeecher@gmail.com>
2018-01-16 17:10:48 -0800

    styling done on google map div

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 //javascript.js
 //set map options
-            var myLatLng = {lat: 51.5, lng: -0.1};
+            var myLatLng = {lat: 47.6062, lng: -122.3321};
             var mapOptions = {
                 center: myLatLng,
-                zoom: 7,
+                zoom: 15,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
 
             };
